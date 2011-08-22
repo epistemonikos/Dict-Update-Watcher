a39046a721ef11a3382d3b03becb3975ad84e188
Sebastian Valenzuela <sebastiandfg@gmail.com>
2011-08-22 10:39:21 -0300

    Added tests for UpdateItObject

--- tests/test_update_it.py
+++ tests/test_update_it.py
@@ -1,11 +1,46 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+from update_it_object import UpdateItObject
+
 
 class TestUpdateIt(unittest.TestCase):
-	def setUp(self):
-		pass
+    def setUp(self):
+        self.dict = {  "info" :
+                               { "doi" : "10.1056/NEJM199803263381303",
+                                 "classification" : "primary-study",
+                                 "author" : [
+                                    "SE. Inzucchi",
+                                    "DG. Maggs",
+                                    "GR. Spollett",
+                                    "SL. Page",
+                                    "FS. Rife",
+                                    "V. Walton",
+                                    "GI. Shulman"
+                                    ],
+                                 "strategy" : None,
+                                 "source" : "PubMed",
+                                 "same_as" : None,
+                                 "pubmed" : "9516221",
+                                 "date" : None,
+                                 "type" : "http://purl.org/ontology/bibo/Document" },
+                       "modified" : 1308190099, "id" : "123" }
+        self.document = UpdateItObject(self.dict)
 
-    
+    def test_load_from_json(self):
+        self.assertEqual(self.dict['info']['classification'],self.document.info.classification)
+
+    def test_update_document(self):
+        self.document.info.classification = 'systematic-review'
+        self.assertEqual('systematic-review',self.document.info.classification)
+
+    def test_updated_fields(self):
+        self.document.info.classification = 'systematic-review'
+        self.assertEqual({'info.classification'},self.document.updated_fields())
+
+
+
+
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- update_it_object/__init__.py
+++ update_it_object/__init__.py
@@ -1,1 +1,5 @@
-#
+# -*- coding: utf-8 -*-
+
+class UpdateItObject(object):
+    def __init__(self,dict):
+        pass
