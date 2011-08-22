62d7955b253bd0c30611bdc33041779d55c07796
Daniel Pérez Rada <dperezrada@gmail.com>
2011-08-22 12:35:17 -0300

    First complete version

--- tests/test_update_it.py
+++ tests/test_update_it.py
@@ -6,38 +6,49 @@ from update_it_object import UpdateItObject
 
 class TestUpdateIt(unittest.TestCase):
     def setUp(self):
-        self.dict = {  "info" :
-                               { "doi" : "10.1056/NEJM199803263381303",
-                                 "classification" : "primary-study",
-                                 "author" : [
-                                    "SE. Inzucchi",
-                                    "DG. Maggs",
-                                    "GR. Spollett",
-                                    "SL. Page",
-                                    "FS. Rife",
-                                    "V. Walton",
-                                    "GI. Shulman"
-                                    ],
-                                 "strategy" : None,
-                                 "source" : "PubMed",
-                                 "same_as" : None,
-                                 "pubmed" : "9516221",
-                                 "date" : None,
-                                 "type" : "http://purl.org/ontology/bibo/Document" },
-                       "modified" : 1308190099, "id" : "123" }
+        self.dict = {
+            "info":
+            {
+                "doi": "10.1056/NEJM199803263381303",
+                "classification": "primary-study",
+                "author": [
+                "SE. Inzucchi",
+                "DG. Maggs",
+                "GR. Spollett",
+                "SL. Page",
+                "FS. Rife",
+                "V. Walton",
+                "GI. Shulman"
+                ],
+                "strategy": None,
+                "source": "PubMed",
+                "same_as": None,
+                "pubmed": "9516221",
+                "date": None,
+                "type": "http://purl.org/ontology/bibo/Document"
+            },
+            "modified": 1308190099,
+            "id": "123"
+        }
         self.document = UpdateItObject(self.dict)
 
     def test_load_from_json(self):
-        self.assertEqual(self.dict['info']['classification'],self.document.info.classification)
+        self.assertEqual(self.dict['info']['classification'], self.document.info.classification)
 
     def test_update_document(self):
         self.document.info.classification = 'systematic-review'
         self.assertEqual('systematic-review',self.document.info.classification)
 
     def test_updated_fields(self):
+        self.assertEqual([], self.document.updated_fields())
         self.document.info.classification = 'systematic-review'
-        self.assertEqual({'info.classification'},self.document.updated_fields())
+        self.assertEqual(['info.classification'],self.document.updated_fields())
 
+    def test_select_field(self):
+        self.assertEqual('primary-study', self.document.get('info.classification'))
+    
+    def test_get_inexistence_field(self):
+        self.assertEqual(None, self.document.get('info.no_existe'))
 
 
 
--- update_it_object/__init__.py
+++ update_it_object/__init__.py
@@ -1,5 +1,59 @@
 # -*- coding: utf-8 -*-
 
 class UpdateItObject(object):
-    def __init__(self,dict):
-        pass
+    def __init__(self, element):
+        self._changed = []
+        if isinstance(element, dict):
+            for key, value in element.iteritems():
+                setattr(self, key, UpdateItObject(value))
+            self._value = None
+            self._leaf = False
+        elif isinstance(element, (list, tuple)):
+            self._value = [UpdateItObject(x) for x in element]
+            self._leaf = False
+        else:
+            self._value = element
+            self._leaf = True
+        self._changed = []
+    
+    def __getattribute__(self, name):
+        element = object.__getattribute__(self, name)
+        if isinstance(element, UpdateItObject):
+            if element._leaf:
+                return element._value
+        return element
+
+    def __setattr__(self, key, value):
+        if key == '_changed':
+            self.__dict__[key] = value
+            return None
+        self.__dict__[key] = value
+        self._changed.append(key)
+    
+    def updated_fields(self, pwd = None):
+        list_ = []
+        for key, value in self.__dict__.iteritems():
+            if key == '_changed':
+                continue
+            if pwd:
+                pwd_current = "%s.%s" % (pwd,key)
+            else:
+                pwd_current = key
+            if isinstance(value, UpdateItObject):
+                if len(value._changed) > 0:
+                    for changed_element in value._changed:
+                        list_.append("%s.%s" % (pwd_current, changed_element))
+                else:
+                    list_.extend(value.updated_fields(pwd_current))
+        return list_
+    
+    def get(self, name):
+        fields = name.split('.')
+        to_return_value = self
+        try:
+            for field in fields:
+                to_return_value = to_return_value.__getattribute__(field)
+            return to_return_value
+        except:
+            return None
+    
